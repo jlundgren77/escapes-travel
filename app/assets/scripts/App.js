2011a5999f35173adf73805f294c4934f74a29ef
jlundgren77 <jlundgren77@gmail.com>
2020-12-31 11:12:18 -0500

    new styles

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,7 @@
+// const scss = require('../scss/index.scss');
+import '../scss/index.scss';
+
+if (module.hot) {
+  module.hot.accept();
+}
+console.log('hello');
---
+++ webpack.config.js
@@ -0,0 +1,99 @@
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+  entry: './app/assets/scripts/App.js',
+  output: {
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'app'),
+  },
+  devServer: {
+    before: function (app, server) {
+      server._watch('./app/**/*.html');
+    },
+    contentBase: path.join(__dirname, 'app'),
+    hot: true,
+    port: 3000,
+    host: '0.0.0.0',
+  },
+  mode: 'development',
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+
+      {
+        // Apply rule for .sass, .scss or .css files
+        test: /\.(sa|sc|c)ss$/,
+
+        // Set loaders to transform files.
+        // Loaders are applying from right to left(!)
+        // The first loader will be applied after others
+        use: [
+          {
+            // After all CSS loaders we use plugin to do his work.
+            // It gets all transformed CSS and extracts it into separate
+            // single bundled file
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            // This loader resolves url() and @imports inside CSS
+            loader: 'css-loader?url=false',
+          },
+
+          {
+            // First we transform SASS to standard CSS
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
+            },
+          },
+        ],
+      },
+      {
+        // Now we apply rule for images
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            // Using file-loader for these files
+            loader: 'file-loader',
+
+            // In options we can set different things like format
+            // and directory to save
+            options: {
+              outputPath: 'images',
+            },
+          },
+        ],
+      },
+      {
+        // Apply rule for fonts files
+        test: /\.(woff|woff2|ttf|otf|eot)$/,
+        use: [
+          {
+            // Using file-loader too
+            loader: 'file-loader',
+            options: {
+              outputPath: 'fonts',
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'bundle.css',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+};
