92f032cedfd491cb7f9854507e1df467cd3f433c
jlundgren77 <jlundgren77@gmail.com>
2021-01-12 15:05:26 -0500

    scroll complete

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -2,11 +2,13 @@
 import '../scss/index.scss';
 import MobileMenu from './modules/MobileMenu';
 import ScrollReveal from './modules/Scroll';
+import StickyHeader from './modules/StickyHeader';
 
 if (module.hot) {
   module.hot.accept();
 }
 
+let stickyHeader = new StickyHeader();
 new ScrollReveal(document.querySelectorAll('.feature-item'), 75);
 new ScrollReveal(document.querySelectorAll('.testimonial'), 60);
 const menu = new MobileMenu();
