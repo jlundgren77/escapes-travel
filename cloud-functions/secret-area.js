675c195b18d80f97829599bcdff89329d6b7c791
jlundgren77 <jlundgren77@gmail.com>
2021-01-26 16:17:48 -0500

    secret area

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,24 @@
 exports.handler = function (event, context, callback) {
-  callback(null, {
-    statusCode: 200,
-    body: 'Welcome to the secret area',
-  });
+  let body;
+  const secretContent = `
+    <h3>Welcome to the secreat area</h3>
+    <p><strong>Necrot</strong> is the best!!!!</p>
+  `;
+
+  if (event.body) {
+    body = JSON.parse(event.body);
+  } else {
+    body = {};
+  }
+
+  if (body.password === 'javascript') {
+    callback(null, {
+      statusCode: 200,
+      body: secretContent,
+    });
+  } else {
+    callback(null, {
+      statusCode: 401,
+    });
+  }
 };
